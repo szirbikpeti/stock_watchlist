28cba101dce02b3c96aa2a121292a4c38e19e3af
Szirbik Péter <h880681@stud.u-szeged.hu>
2021-02-27 16:54:23 +0100

    Upload files

---
+++ Procfile
@@ -0,0 +1,12 @@
+aenum==2.2.6
+attrs==20.3.0
+beautifulsoup4==4.9.3
+certifi==2020.12.5
+chardet==4.0.0
+fbchat==1.9.7
+idna==2.10
+paho-mqtt==1.5.1
+requests==2.25.1
+soupsieve==2.2
+urllib3==1.26.3
+worker: python script.py
---
+++ script.py
@@ -0,0 +1,58 @@
+#from time import sleep
+
+#from yahoo_fin import stock_info as si
+
+from fbchat import Client
+from fbchat.models import Message, ThreadType
+
+#from tabulate import tabulate
+
+base_path = "C:\\Users\\Vostro 15\\Google Drive\\Egyetem_SZTE\\5_felev\\Szkriptnyelvek\\gyakorlat\\tobbi"
+my_thread_id = 100002404483520
+
+
+class MessageBot(Client):
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+        if (int(thread_id) == my_thread_id) and message_object.text == '?':
+            msg_id = self.send(Message(text="Processing..."), thread_id=my_thread_id, thread_type=ThreadType.USER)
+            get_buyable_stocks(my_client)
+            self.deleteMessages(msg_id)
+
+
+def get_buyable_stocks(client: Client):
+    closest_stock = ("", 99999, 0, 0)
+
+    with open(f"{base_path}\\stock_price_target.csv", "r") as file:
+        next(file)
+        line = file.readline().split(";")
+
+        stock_details = []
+
+        while line[0] != '':
+            diff = float(si.get_live_price(line[0])) - float(line[1])
+            if closest_stock[1] > diff > 0:
+                closest_stock = (line[0].upper(), diff, round(float(si.get_live_price(line[0])), 2), float(line[1]))
+
+            if float(si.get_live_price(line[0])) < float(line[1]):
+                stock_details.append([line[0].upper(), round(float(si.get_live_price(line[0])), 2), float(line[1])])
+
+            line = file.readline().split(";")
+
+    print("Sent!") if len(stock_details) > 0 else print("Nothing to show!")
+
+    stock_details.append([f"({closest_stock[0]})", closest_stock[2], closest_stock[3]])
+
+    client.send(
+        Message(text=f"{tabulate(stock_details, headers=['Ticker', 'CPrice', 'TPrice'], tablefmt='presto')}"),
+        thread_id=my_thread_id, thread_type=ThreadType.USER)
+
+    client.markAsUnread(my_thread_id)
+
+    print(f"The closest: {closest_stock[0]} (diff: {closest_stock[1]})")
+    sleep(5)
+
+
+my_client = Client("szirbikpeti@gmail.com", "tnkEelxr7rfpsf5v1f09Ky23b86f810"[1::3])
+#get_buyable_stocks(my_client) if int(input("Branch: ")) == 0 else my_client.listen()
+my_client.send(Message(text="Successful deploy!"), thread_id=my_thread_id, thread_type=ThreadType.USER)
+my_client.markAsUnread(my_thread_id)
