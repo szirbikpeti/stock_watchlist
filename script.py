8f948cfb3a2b2e7d67e4cb2aeefc194c648b8828
Szirbik Péter <szirbikpeti@gmail.com>
2021-03-07 20:49:18 +0100

    Send buyable stocks as image

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from tabulate import tabulate
+from yahoo_fin import stock_info as si
+from PIL import Image, ImageDraw
 
 
 GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google-chrome'
@@ -37,9 +39,42 @@ def check_request(thread_id: int):
     messages = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[1]/div[2]/div/div/div/div[1]/div[1]/div/div/div[3]/div')
 
     if messages.text[-1] == '?':
-        new_message = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/form/div/div[3]/div[2]/div[1]/div/div/div/div/div[2]/div/div/div/div')
-        new_message.send_keys('Send info!')
-        new_message.send_keys(Keys.ENTER)
+        input_field = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/form/div/div[3]/div[2]/div[1]/div/div/div/div/div[2]/div/div/div/div')
+
+        get_buyable_stocks()
+		
+        image = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/form/div/div[3]/div[1]/input')
+        image.send_keys('/app/buyable_stocks.png')
+		
+        input_field.send_keys(Keys.ENTER)
+
+
+def get_buyable_stocks():
+    closest_stock = ("", 99999, 0, 0)
+
+    with open(f"{base_path}\\stock_price_target.csv", "r") as file:
+        next(file)
+        line = file.readline().split(";")
+
+        stock_details = []
+
+        while line[0] != '':
+            diff = float(si.get_live_price(line[0])) - float(line[1])
+            if closest_stock[1] > diff > 0:
+                closest_stock = (line[0].upper(), diff, round(float(si.get_live_price(line[0])), 2), float(line[1]))
+
+            if float(si.get_live_price(line[0])) < float(line[1]):
+                stock_details.append([line[0].upper(), round(float(si.get_live_price(line[0])), 2), float(line[1])])
+
+            line = file.readline().split(";")
+
+    stock_details.append([f"({closest_stock[0]})", closest_stock[2], closest_stock[3]])
+
+    img = Image.new('RGB', (215, 58 + (len(stock_details) - 1) * 14 + 5), color=(73, 109, 137))
+    d = ImageDraw.Draw(img)
+    d.text((10, 10), f"{tabulate(stock_details, headers=['Ticker', 'CPrice', 'TPrice'], tablefmt='presto')}",
+           fill=(255, 255, 0))
+    img.save('/app/buyable_stocks.png')
 
 
 while True:
