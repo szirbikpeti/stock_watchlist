4cd58f917caa01933a2c1a94ff89be882d1612c3
Szirbik Péter <szirbikpeti@gmail.com>
2021-03-07 18:01:15 +0100

    Try selenium

--- script.py
+++ script.py
@@ -1,61 +1,46 @@
-import pathlib
-from time import sleep
-
-from yahoo_fin import stock_info as si
-
-from fbchat import Client
-from fbchat.models import Message, ThreadType
-
-from tabulate import tabulate
-
-my_thread_id = 0
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 
 
-class MessageBot(Client):
-    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
-        if (int(thread_id) == 100002404483520 or int(thread_id) == 100000656116842) and message_object.text == '?':
-            global my_thread_id
-            my_thread_id = thread_id
-            msg_id = self.send(Message(text="Processing..."), thread_id=my_thread_id, thread_type=ThreadType.USER)
-            get_buyable_stocks(my_client)
-            print(self.deleteMessages(msg_id))
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
-def get_buyable_stocks(client: Client = None):
-    closest_stock = ("", 99999, 0, 0)
-
-    with open(str(pathlib.Path(__file__).parent.absolute()) + "/stock_price_target.csv", "r") as file:
-        next(file)
-        line = file.readline().split(";")
-
-        stock_details = []
-
-        while line[0] != '':
-            diff = float(si.get_live_price(line[0])) - float(line[1])
-            if closest_stock[1] > diff > 0:
-                closest_stock = (line[0].upper(), diff, round(float(si.get_live_price(line[0])), 2), float(line[1]))
-
-            if float(si.get_live_price(line[0])) < float(line[1]):
-                stock_details.append([line[0].upper(), round(float(si.get_live_price(line[0])), 2), float(line[1])])
-
-            line = file.readline().split(";")
-
-    print("Sent!") if len(stock_details) > 0 else print("Nothing to show!")
-
-    stock_details.append([f"({closest_stock[0]})", closest_stock[2], closest_stock[3]])
-
-    if client is not None:
-        client.send(
-            Message(text=f"{tabulate(stock_details, headers=['Ticker', 'CPrice', 'TPrice'], tablefmt='presto')}"),
-            thread_id=my_thread_id, thread_type=ThreadType.USER)
-
-        client.markAsUnread(my_thread_id)
-    else:
-        print(f"{tabulate(stock_details, headers=['Ticker', 'CPrice', 'TPrice'], tablefmt='presto')}")
-
-    print(f"The closest: {closest_stock[0]} (diff: {closest_stock[1]})")
-    sleep(10)
+driver.get('https://www.messenger.com/')
 
 
-my_client = MessageBot("stockswatcher21@gmail.com", "stockSender21", max_tries=1)
-my_client.listen()
+accept_cookies_button = driver.find_element_by_xpath(
+    '/html/body/div[2]/div[2]/div/div/div/div/div[3]/button[2]')
+accept_cookies_button.click()
+
+
+email_input = driver.find_element_by_xpath(
+    '/html/body/div/div/div/div[1]/div/div/div/div[1]/div/div[3]/div/div[7]/div[1]/div/div[2]/div[1]/div/form/div/input[6]')
+email_input.send_keys('stockswatcher21@gmail.com')
+
+password_input = driver.find_element_by_xpath(
+    '/html/body/div/div/div/div[1]/div/div/div/div[1]/div/div[3]/div/div[7]/div[1]/div/div[2]/div[1]/div/form/div/input[7]')
+password_input.send_keys('stockSender21')
+
+login_button = driver.find_element_by_xpath(
+    '/html/body/div/div/div/div[1]/div/div/div/div[1]/div/div[3]/div/div[7]/div[1]/div/div[2]/div[1]/div/form/div/div[1]/button')
+login_button.click()
+
+driver.implicitly_wait(10)
+
+grid = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div/div/div[3]/div[1]/div[2]/div/div')
+print(grid.text)
+
+while True:
+    new_message_sign = driver.find_element_by_xpath(
+        '/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div/div/div[3]/div[1]/div[2]/div/div/div[1]/div/div[1]/div/a/div/div[2]/div[1]/div/div/div[2]/span/span/div/span[3]')
+
+    if new_message_sign.text == '1 p.':
+        mine = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div/div/div[3]/div[1]/div[2]/div/div/div[1]/div/div[1]/div/a/div[1]/div[2]/div[1]/div/div')
+        mine.click()
+
+        new_message = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/form/div/div[3]/div[2]/div[1]/div/div/div/div/div[2]/div/div/div/div')
+        new_message.send_keys('Successful automatization!')
+        new_message.send_keys(Keys.ENTER)
+
+
