9effd1cee35c57ee5be05d8559703d1b564f7547
szirbikpeti <61188059+szirbikpeti@users.noreply.github.com>
2021-03-07 18:55:05 +0100

    Remove chrome options

--- script.py
+++ script.py
@@ -6,14 +6,7 @@ from selenium.webdriver.common.keys import Keys
 GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.binary_location = GOOGLE_CHROME_PATH
-
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH)
 driver.get('https://www.messenger.com/')
 
 
