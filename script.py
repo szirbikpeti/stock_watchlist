24cfce7635432df2706c670fa64e0f5f969c1608
Szirbik Péter <szirbikpeti@gmail.com>
2021-03-08 16:34:49 +0100

    Add forex feature

--- script.py
+++ script.py
@@ -7,16 +7,23 @@ from tabulate import tabulate
 
 from PIL import Image, ImageDraw
 
-import os
+from forex_python.converter import CurrencyRates
+from forex_python.bitcoin import BtcConverter
 
 
 class MessageBot(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
-        if (int(thread_id) == 100002404483520 or int(thread_id) == 100000656116842) and (message_object.text == '?' or message_object.text == '?p'):
-            print(f'Request from {my_client.fetchUserInfo(thread_id)[str(thread_id)].name}')
-            msg_id = self.send(Message(text="Processing..."), thread_id=thread_id, thread_type=ThreadType.USER)
-            sender(my_client, thread_id, get_buyable_stocks(), message_object.text == '?')
-            self.deleteMessages(msg_id)
+        if int(thread_id) == 100002404483520 or int(thread_id) == 100000656116842 and isinstance(message_object.text, str):
+            if message_object.text.lower() == 'usd':
+                sender(self, thread_id, CurrencyRates().get_rate('USD', 'HUF'))
+            elif message_object.text.lower() == 'eur':
+                sender(self, thread_id, CurrencyRates().get_rate('EUR', 'HUF'))
+            elif message_object.text.lower() == 'btc':
+                sender(self, thread_id, BtcConverter().get_latest_price('USD'))
+            elif message_object.text == '?' or message_object.text == '?p':
+                msg_id = self.send(Message(text="Processing..."), thread_id=thread_id, thread_type=ThreadType.USER)
+                sender(self, thread_id, get_buyable_stocks(), message_object.text == '?')
+                self.deleteMessages(msg_id)
 
 
 def get_buyable_stocks():
@@ -43,9 +50,9 @@ def get_buyable_stocks():
     return f"{tabulate(stock_details, headers=['Ticker', 'CPrice', 'TPrice'], tablefmt='presto')}"
 
 
-def sender(client: Client, thread_id: int, message: str, isTextFormat: bool):
-    client.send(Message(text=message), thread_id=thread_id) if isTextFormat else client.sendLocalImage(get_image(message), thread_id=thread_id)
-    print('Image sent!')
+def sender(client: Client, thread_id: int, message: str, is_text: bool = True):
+    print(f'Request from {client.fetchUserInfo(thread_id)[str(thread_id)].name}')
+    client.send(Message(text=message), thread_id=thread_id) if is_text else client.sendLocalImage(get_image(message), thread_id=thread_id)
 
 
 def get_image(message: str):
@@ -53,12 +60,11 @@ def get_image(message: str):
     d = ImageDraw.Draw(img)
     d.text((10, 10), message, fill=(255, 255, 0))
     path = '/app/image.png'
-    print(os.path.exists(path))
     img.save(path)
-    print(os.path.exists(path))
 
     return path
 
 
 my_client = MessageBot("stockswatcher21@gmail.com", "stockSender21", max_tries=1, user_agent='[FB_IAB/MESSENGER;FBAV/310.0.0.0.83;]')
 my_client.listen()
+
