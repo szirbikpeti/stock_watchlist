cc4c28441f747226a7f21ec165654527bcc3ec3e
Szirbik Péter <h880681@stud.u-szeged.hu>
2021-04-15 15:29:50 +0200

    Get fb version with selenium

--- selenium_script.py
+++ selenium_script.py
@@ -21,40 +21,13 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.binary_location = GOOGLE_CHROME_PATH
 
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-driver.get('https://www.messenger.com/')
+driver.get('https://www.androidapksbox.com/facebook/')
 
-try:
-	accept_cookies_button = driver.find_element_by_xpath(
-		'/html/body/div[2]/div[2]/div/div/div/div/div[3]/button[2]')
-	accept_cookies_button.click()
-except:
-	pass
+table = driver.find_element_by_xpath(
+    '/html/body/div[1]/div[2]/div/article/div[1]/p[2]')
 
-email_input = driver.find_element_by_xpath(
-    '/html/body/div/div/div/div[1]/div/div/div/div[1]/div/div[3]/div/div[7]/div[1]/div/div[2]/div[1]/div/form/div/input[6]')
-email_input.send_keys('stockswatcher21@gmail.com')
+version = [row[9:].split(" (")[0] for row in table.text.split("\n") if str(row).startswith("Version")][0]
 
-password_input = driver.find_element_by_xpath(
-    '/html/body/div/div/div/div[1]/div/div/div/div[1]/div/div[3]/div/div[7]/div[1]/div/div[2]/div[1]/div/form/div/input[7]')
-password_input.send_keys('stockSender21')
-password_input.send_keys(Keys.ENTER)
-
-driver.implicitly_wait(10)
-
-
-def check_request(thread_id: int):
-    driver.get(f'https://www.messenger.com/t/{thread_id}')
-    messages = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[1]/div[2]/div/div/div/div[1]/div[1]/div/div/div[3]/div')
-
-    if messages.text[-1] == '?':
-        input_field = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/form/div/div[3]/div[2]/div[1]/div/div/div/div/div[2]/div/div/div/div')
-
-        input_field.send_keys("Successful!")
-        input_field.send_keys(Keys.ENTER)
-
-
-while True:
-    check_request(100002404483520)
-    check_request(100000656116842)
+print(version)
 
 
